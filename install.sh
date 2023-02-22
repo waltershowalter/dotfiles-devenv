be3a10ab7f0ef8594fc61e08f1494b1d71917423
Andres Fernandez <andresfernandez@Andres-MacBook-Nefeli.local>
2023-02-21 17:47:34 -0700

    using newer repo created Matthew

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -ex
 cp ssh/authorized_keys ~/.ssh/authorized_keys
 export DEFAULT_ORGUNITS_ALERTS_EMAIL='andres.fernandez@nefeli.io'
+# place dotfiles
+rm -rf ~/.dotfiles
+mv ~/dotfiles ~/.dotfiles
+ln -fs ~/.dotfiles/dotsyncrc ~/.dotsyncrc
+cd ~/.dotfiles
+./dotsync -I
+./dotsync -L
+
